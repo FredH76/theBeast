54a96609aeeb639b8ff5ef81a67de21f1f645ddb
Frederic Huguenin <fred76temp@gmail.com>
2018-06-03 10:51:47 +0200

    fix: fix flickering on first map display due to initial image resizing

--- src/directives/full-height/full-height.ts
+++ src/directives/full-height/full-height.ts
@@ -1,11 +1,5 @@
 import { Directive, ElementRef, Renderer } from '@angular/core';
 
-/**
- * Generated class for the FullHeightDirective directive.
- *
- * See https://angular.io/api/core/Directive for more info on Angular
- * Directives.
- */
 @Directive({
   selector: '[full-height]' // Attribute selector
 })
@@ -18,29 +12,40 @@ export class FullHeightDirective {
   }
 
   /**
-   * function called after view is checked (several times after ngAfterViewInit())
+   * function to automatically adjust image height to vertical space available
    */
-  ngAfterViewChecked() {
-    //get the image container
-    let imageContainer = this.findContainer(this.el);
+  ngAfterViewInit() {
+    let height: number;
 
-    //get height of the image container
-    let num = imageContainer.clientHeight.toString();
+    //get the image container
+    let imageContainer = this.findImageContainerof(this.el);
+    let contHeight = imageContainer.clientHeight;
+
+    //get height of navBar
+    let navbarElt = document.getElementsByClassName("toolbar");
+    let navHeight = navbarElt[0].clientHeight;
+
+    //compute final heigth
+    height = contHeight - navHeight;
 
     //set height of the image
-    this.renderer.setElementStyle(this.el, 'height', num + "px");
-    console.log(this.el.parentNode.parentNode.clientHeight);
+    this.renderer.setElementStyle(this.el, 'height', height + "px");
   }
+
 
   /** 
     * search for the closest element with the class "imageContainer"
     */
-  findContainer(el) {
-    let orignialEl = el;
+  findImageContainerof(el) {
+    let orignialElt = el;
+
+    // loop to get to parent element automatically
     while ((el = el.parentElement) && (el != null) && !el.classList.contains("imageContainer"));
+
+    // return container element or original element (if no cmatch found)
     if (el == null) {
       console.warn("you need to use an element with a 'imageContainer' class when using 'full-height' directive");
-      return orignialEl;
+      return orignialElt;
     }
     else
       return el;
